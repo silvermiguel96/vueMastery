9bc6d79c4d30e73c68e5270ff6038dc9ba3b5eee
Miguelangel <silvermiguel96@gmail.com>
2019-06-21 17:09:38 -0500

    Components

--- introtovue.js/Lessons/main.js
+++ introtovue.js/Lessons/main.js
@@ -1,25 +1,64 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    brand: 'Vue Mastery',
-    product: 'Socks',
-    selectedVariant: 0,
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      { 
-        variantID: 2234,
-        variantColor: "green",
-        variantImage: './assets/vmSocks-green-onWhite.jpg',
-        variantQuantity: 10
-      },
-      { 
-        variantID: 2235,
-        variantColor: "blue",
-        variantImage: './assets/vmSocks-blue-onWhite.jpg',
-        variantQuantity: 0
-      }
-    ],
-    cart: 0
+Vue.component('product', {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true
+    }
+  },
+  template: `
+  <div class="product">
+  <div class="product-image">
+    <img v-bind:src="image" alt="">
+  </div>
+  <div class="product-info">
+    <h1>{{title}}</h1>
+    <p v-if="inStock">In Stock</p>
+    <p v-else>Out of Stock</p>
+    <p>Shipping:  {{ shipping }}</p>
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+
+    <div v-for="(variant, index) in variants"
+         :key="variant.variantId"
+         class="color-box"
+         :style="{ backgroundColor: variant.variantColor }"
+         v-on:mouseover="updateProduct(index)">
+    </div>
+
+    <button v-on:click="addToCart"
+            :disabled="!inStock"
+            :class="{ disabledButton: !inStock }"
+    >Add to Cart</button>
+
+    <div class="cart">
+      <p>Cart (({{cart}}))</p>
+    </div>
+  </div>
+</div>
+  `,
+  data() {
+    return {
+
+      brand: 'Vue Mastery',
+      product: 'Socks',
+      selectedVariant: 0,
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [{
+          variantID: 2234,
+          variantColor: "green",
+          variantImage: './assets/vmSocks-green-onWhite.jpg',
+          variantQuantity: 10
+        },
+        {
+          variantID: 2235,
+          variantColor: "blue",
+          variantImage: './assets/vmSocks-blue-onWhite.jpg',
+          variantQuantity: 0
+        }
+      ],
+      cart: 0
+    }
   },
   methods: {
     addToCart: function () {
@@ -39,6 +78,19 @@ var app = new Vue({
     },
     inStock() {
       return this.variants[this.selectedVariant].variantQuantity
+    },
+    shipping() {
+      if (this.premium) {
+        return "Free"
+      }
+      return "2.99"
     }
+  }
+})
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    premium: true
   }
 })
